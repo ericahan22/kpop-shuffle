39346ab42562a943f6683fa3f67c278ca309c998
chewtle <57574635+chewtle@users.noreply.github.com>
2021-05-03 18:45:43 -0400

    Update config.js
    Replaced MusicBrainzApi with API_KEY

--- config.js
+++ config.js
@@ -1,26 +1,3 @@
-import {MusicBrainzApi} from '../src/musicbrainz-api';
-
-const config = {
-  // MusicBrainz bot account username & password (optional)
-  botAccount: { 
-    username: 'myUserName_bot',
-    password: 'myPassword' 
-  },
-  
-  // API base URL, default: 'https://musicbrainz.org' (optional)
-  baseUrl: 'https://musicbrainz.org',
-
-  appName: 'my-app',
-  appVersion: '0.1.0',
-
-  // Optional, default: no proxy server
-  proxy: {
-    host: 'localhost',
-    port: 8888
-   },
-
-  // Your e-mail address, required for submitting ISRCs
-  appMail: "ericahan.38@gmail.com"
+var config = {
+  API_KEY: "AIzaSyBWHjs9S0Qq9K2RPVOG37gPi7d8BTbVau0"
 }
-
-const mbApi = new MusicbrainzApi(config);
